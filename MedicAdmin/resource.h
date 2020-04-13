2496fb2d49effce512959dcb4fce99df7a24c5b0
LL <576484879@qq.com>
2020-04-13 20:14:58 +0800

    儿童宝宝

--- MedicAdmin/resource.h
+++ MedicAdmin/resource.h
@@ -39,13 +39,14 @@
 #define ID_32777                        32777
 #define ID_32778                        32778
 #define ID_32779                        32779
+#define ID_32780                        32780
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        327
-#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
